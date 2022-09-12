4ea194e74e6379e4022ebd5881c9856854af5078
caldasdeoliveira <diogo@loka.com>
2022-09-12 12:27:28 +0100

    simplify training script

--- train.py
+++ train.py
@@ -2,31 +2,34 @@ import pandas as pd
 import numpy as np
 
 from pathlib import Path
+import argparse
 
 import mlflow
 
 from model import instantiate_pipeline
 
-from load_data import read_dataframe, X_y_split
-
 # see available datasets
-data_dir = Path("data")
-dataset_file_list = [str(x) for x in list(data_dir.glob("*.parquet"))]
-dataset_file_list.sort()
+# data_dir = Path("data")
+# dataset_file_list = [str(x) for x in list(data_dir.glob("*.parquet"))]
+# dataset_file_list.sort()
 
-print(dataset_file_list)
+# print(dataset_file_list)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--X_dataset", "-X", type=str)
+parser.add_argument("--y_dataset", "-y", type=str)
+
+args = parser.parse_args()
+
+X_path = args.X_dataset
+y_path = args.y_dataset
 
 # training
 with mlflow.start_run() as run:
     mlflow.sklearn.autolog()
-    df = read_dataframe(dataset_file_list[-1])
-    
-    df = df[['VendorID', 'tpep_pickup_datetime',
-             'trip_distance', 'PULocationID', 'DOLocationID', 'duration',]]
-    #TODO move this to a better location
-    
-    X, y = X_y_split(df)
-    
+    X = pd.read_parquet(X_path)
+    y = pd.read_parquet(y_path)
+
     clf = instantiate_pipeline(X)
 
     clf.fit(X, y)
